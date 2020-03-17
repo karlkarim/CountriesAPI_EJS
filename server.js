e55d3a9b74e58502a8509fc86b430e93f4d96357
Kevin S <kevinolisiin@gmail.com>
2020-03-17 21:39:49 +0200

    got request data to show

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require ('body-parser');
 const app = express();
 const request = require('request');
+const axios = require("axios");
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
@@ -10,20 +11,40 @@ app.set('views', 'views');
 
 
 
-app.use(function(req,res,next){
-    res.render('index', {
-        pageTitle: "Home page",
-        pageNotFound: "Oops. Page Not Found. Try something else",
-        pageTest: "Page Test",
-        path: ""
-    });
-    //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    //console.log(path.join(__dirname, 'views', '404.html'))
-})
+app.post("/", function (req, res) {
 
-app.get("/", function(req,res){
-    res.sendFile(__dirname+"views/index.ejs");
-    console.log(__dirname);
+	let currency = req.body.currency;
+
+	axios
+		.get(`https://restcountries.eu/rest/v2/name/${currency}?fullText=true`)
+		.then(response => {
+
+			res.render("index", {
+				pageTitle: "Home",
+				pageNotFound: "Oops. Page Not Found. Try something else",
+				pageTest: "Page Test",
+				path: "",
+				data: response.data[0]
+			});
+
+		})
+		.catch(error => {
+			console.log(error);
+		});
+
+});
+
+
+app.get("/", function (req, res) {
+	res.render("index", {
+		pageTitle: "Home page",
+		pageNotFound: "Oops. Page Not Found. Try something else",
+		pageTest: "Page Test",
+		path: "",
+		data: {
+			name: null
+		}
+	});
 });
 
 
